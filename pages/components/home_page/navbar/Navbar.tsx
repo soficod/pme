0832fbd3cbe4e2f46a1b530c2166608231f57f2f
soficod <58166871+soficod@users.noreply.github.com>
2021-08-11 17:57:56 +0200

    Add files via upload

---
+++ pages/components/home_page/navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../../../../styles/landing_page/Navbar.module.css';
+
+const Navbar = () => {
+    return(
+        <h1 className={styles.test}>Test</h1>
+    )
+}
+
+export default Navbar;
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Navbar from './components/home_page/navbar/Navbar';
+
+const Home = () => {
+  return (
+    <Navbar/>
+  );
+}
+
+export default Home;
