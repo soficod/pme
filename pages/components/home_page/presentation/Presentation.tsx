cad7d242d5cfefcc3db44080712d868d956dffa9
Anis ROUANE <35432598+ZaPuncakeu@users.noreply.github.com>
2021-08-17 22:59:36 +0200

    Updated Landing page

--- pages/components/home_page/presentation/Presentation.tsx
+++ pages/components/home_page/presentation/Presentation.tsx
@@ -1,15 +1,63 @@
-import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
-import { useLanguage } from '../../../services/useLanguage';
+import React, { useContext } from 'react'
+import { langContext } from '../../../services/langContext';
 import styles from '../../../../styles/landing_page/Presentation.module.css';
+import {motion} from 'framer-motion';
 
 const Presentation = ()=>{
-    
-    const [lang, changeLanguage] = useLanguage();
+
+    const {lang, changeLanguage} = useContext(langContext);
+
     return(
+        <>
+       
+        <div className={styles.presentationContainer}>
+            <div className={styles.shape}></div>
+
+            <div className={styles.arrow}></div>
+            <div className={styles.arrowhite}></div>
+            <div className={styles.maincontainer}>
+            <div className={styles.box}></div> 
+                <motion.div 
+                    className={styles.container}
+                    initial={{
+                        x:100,
+                        opacity:0
+                    }}
+                    animate ={{
+                        x:0,
+                        opacity:1
+                    }}
+                >
+                    <h1>{lang.Presentation.title}</h1>
+                    <div className={styles.vide}></div>
+                    <p>{lang.Presentation.text}</p>
+                    
+                </motion.div>  
+                
+
+                <motion.div 
+                    className={styles.imageContainer }
+                    initial={{
+                        x:-100,
+                        opacity:0
+                    }}
+                    animate ={{
+                        x:0,
+                        opacity:1
+                    }}
+                >
+                    <img className={styles.image_1} src="images/Rectangle.png"/>
+                    <img className={styles.imglobal} src="images/woman.png"/>
+                    <img className={styles.image_2} src="images/Tracé.png"/>
+            
+                </motion.div>
+            
         
-        <h1>{lang.Navbar.news_link}</h1>
+            </div>
+            </div>
+            <div className={styles.foot}></div>
+        </>
+        
     )
 }
 export default Presentation;
--- pages/index.tsx
+++ pages/index.tsx
@@ -25,6 +25,7 @@ const Home = () => {
       >
         <Navbar/>
         <Main />
+        <Presentation />
         <Mission />
       </langContext.Provider>
     </>
