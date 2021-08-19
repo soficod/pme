b347ad096a7b10fd440f4f716f7715e84431cfcb
Anis ROUANE <35432598+ZaPuncakeu@users.noreply.github.com>
2021-08-19 02:15:24 +0200

    Mission section added

--- pages/components/home_page/mission/Mission.tsx
+++ pages/components/home_page/mission/Mission.tsx
@@ -1,23 +1,148 @@
 import React, { useState, useContext } from 'react';
 import { langContext } from '../../../services/langContext';
 import styles from '../../../../styles/landing_page/Mission.module.css';
+import { motion } from 'framer-motion';
 
 const Mission = () => 
 {
+    const selectedBtnStyle = {
+        backgroundColor: "#58C4DD",
+        color:"white",
+        boxShadow: "0px 0px 36px 1px #58C4DD"
+    }
+    
+
+    const missions = [
+        {
+            title: "Mission 1",
+            content: `
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit.
+            `
+        },
+        {
+            title: "Mission 2",
+            content: `
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit.
+            `
+        },
+        {
+            title: "Mission 3",
+            content: `
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit.
+            `
+        }
+    ]
+
     const {lang, changeLanguage} = useContext(langContext);
+    const [selected, setSelected] = useState(-1);
 
     return(
         <section className={styles.mission_section}>
-            <h1 className={styles.mission_title}>
-                {lang.Mission.title}
-            </h1>
-            <div 
-                className="separator"
-                style={{
-                    margin: "auto"
-                }}
-            >
+            <div className={styles.whiteBg}></div>
+            <div style={{
+                position:'relative',
+                zIndex: 250
+            }}>
+                <h1 className={styles.mission_title}>
+                    {lang.Mission.title}
+                </h1>
                 
+                <div 
+                    className="separator"
+                    style={{
+                        margin: "auto"
+                    }}
+                >
+                </div>
+
+                <div className={styles.bigCircle}>
+                    <div className={styles.icoContainer}>
+                        <motion.button 
+                            style={selected === 1 ? selectedBtnStyle:{}} 
+                            className={styles.ico} 
+                            onClick={e => setSelected(1)}
+                            initial={{
+                                scale: 0
+                            }}
+                            animate={{
+                                scale: 1
+                            }}
+                            transition={{
+                                duration: 1,
+                                delay: 1
+                            }}
+                        >
+                            <span  className="material-icons">
+                                campaign
+                            </span>
+                        </motion.button>
+                    </div>
+
+                    <div className={styles.btnsContainer} style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                    }}>
+                        <motion.button 
+                            style={selected === 0 ? selectedBtnStyle:{}} 
+                            className={styles.ico}
+                            onClick={e => setSelected(0)}
+                            initial={{
+                                scale: 0
+                            }}
+                            animate={{
+                                scale: 1
+                            }}
+                            transition={{
+                                duration: 1,
+                            }}
+                        >
+                            <span className="material-icons">
+                                business_center
+                            </span>
+                        </motion.button>
+
+                        <motion.button 
+                            style={selected === 2 ? selectedBtnStyle:{}} 
+                            className={styles.ico}
+                            onClick={e => setSelected(2)}
+                            initial={{
+                                scale: 0
+                            }}
+                            animate={{
+                                scale: 1
+                            }}
+                            transition={{
+                                duration: 1,
+                                delay: 1.5
+                            }}
+                        >
+                            <span className="material-icons">
+                                handyman
+                            </span>
+                        </motion.button>
+                    </div>
+
+                    <div  className={styles.textContainer}>
+                        {
+                            selected !== -1 && 
+                            <motion.div>
+                                <h1>
+                                    {missions[selected].title}
+                                </h1>
+
+                                <p>
+                                    {missions[selected].content}
+                                </p>
+                            </motion.div>
+                        }
+                    </div>
+                </div>
             </div>
         </section>
     )
