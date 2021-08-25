0bf909af0ec2c3e3855c19d3d845187f03023d9e
Anis ROUANE <35432598+ZaPuncakeu@users.noreply.github.com>
2021-08-26 00:18:39 +0200

    Number section fixed

--- pages/components/home_page/numbers/Numbers.tsx
+++ pages/components/home_page/numbers/Numbers.tsx
@@ -4,36 +4,38 @@ import { langContext } from '../../../services/langContext';
 import Counter from './Counter';
 const Numbers = () => 
 {
-    const {lang, changeLanguage} = useContext(langContext);
+    const {lang, changeLanguage}:any = useContext(langContext);
     return (
         <>
             <div className={styles.numContainer}>
-                <div className={styles.part1}>
-                    <button>
-                        <Counter speed={1} maxCount={200} />
-                        <br />
-                        {lang.Numbers.signed}
-                    </button>
+                <div className={styles.darkBg}>
+                    <div className={styles.part1}>
+                        <button>
+                            <span className={styles.num}><Counter speed={1} maxCount={200} /></span>
+                            <br />
+                            {lang.Numbers.signed}
+                        </button>
 
-                    <button>
-                        <Counter speed={1} maxCount={500} />
-                        <br />
-                        {lang.Numbers.docs}
-                    </button>
-                </div>
+                        <button>
+                            <span className={styles.num}><Counter speed={1} maxCount={500} /></span>
+                            <br />
+                            {lang.Numbers.docs}
+                        </button>
+                    </div>
 
-                <div className={styles.part1}>
-                    <button>
-                        +<Counter speed={1} maxCount={1000} />
-                        <br />
-                        {lang.Numbers.events}
-                    </button>
+                    <div className={styles.part1}>
+                        <button>
+                            <span className={styles.num}>+<Counter speed={1} maxCount={1000} /></span>
+                            <br />
+                            {lang.Numbers.events}
+                        </button>
 
-                    <button>
-                        <Counter speed={1} maxCount={40} />
-                        <br />
-                        {lang.Numbers.sponsors}
-                    </button>
+                        <button>
+                            <span className={styles.num}><Counter speed={1} maxCount={40} /></span>
+                            <br />
+                            {lang.Numbers.sponsors}
+                        </button>
+                    </div>
                 </div>
             </div>
         </>
